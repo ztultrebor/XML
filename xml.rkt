59ddf87ca6e3a88eabcb01ba174a525a01d44a99
Robert Lutz <robertlutzi@gmail.com>
2024-01-04 22:32:28 -0500

    fix bug with symbol-head-body lists

--- xml.rkt
+++ xml.rkt
@@ -41,21 +41,22 @@
   ; takes an X-Expression and converts it into XML
   (match xexpr
     [(? empty?) ""]
+    [(? attribute?) (string-append " " (symbol->string (first xexpr)) "=\""
+                                   (second xexpr) "\"")]
+    [(? head?) (string-append (parse (first xexpr)) (parse (rest xexpr)))]
     [(list (? symbol?))
      (string-append "<" (symbol->string (first xexpr)) " />")]
-    [(? attribute?) (string-append " " (symbol->string (first xexpr)) "=\""
-                              (second xexpr) "\"")]
-    [(? head?) (string-append (parse (first xexpr)) (parse (rest xexpr)))]
     [(list (? symbol?) (? head?))
      (string-append "<" (symbol->string (first xexpr))
                     (parse (second xexpr)) " />")]
-    [(list (? symbol?) bd) (string-append "<" (symbol->string (first xexpr))
-                                          ">" (parse bd)
-                                          "</" (symbol->string (first xexpr)) ">")]    
-    [(list sym hd bd) (string-append "<" (symbol->string sym) ">"
-                                     (parse hd) (parse bd)
-                                     "</" (symbol->string sym) ">")]
-    [(list f r) (string-append (parse f) (parse r))]))
+    [(cons (? symbol?) (cons (? head?) rem))
+     (string-append "<" (symbol->string (first xexpr)) (parse (second xexpr))
+                    ">" (parse rem)
+                    "</" (symbol->string (first xexpr)) ">")]
+    [(cons (? symbol?) rem)
+     (string-append "<" (symbol->string (first xexpr)) ">" (parse rem)
+                    "</" (symbol->string (first xexpr)) ">")]
+    [(? list?) (string-append (parse (first xexpr)) (parse (rest xexpr)))]))
 
 
 
@@ -63,6 +64,17 @@
 ; checks
 
 (check-expect (head? '((from "seen-e") (to "seen-f"))) #t)
+(define a0 '((initial "X")))
+(define e0 '(machine))
+(define e1 `(machine ,a0))
+(define e2 '(machine (action)))
+(define e3 '(machine () (action)))
+(define e4 `(machine ,a0 (action) (action)))
+(check-expect (parse e0) "<machine />")
+(check-expect (parse e1) "<machine initial=\"X\" />")
+(check-expect (parse e2) "<machine><action /></machine>")
+(check-expect (parse e3) "<machine><action /></machine>")
+(check-expect (parse e4) "<machine initial=\"X\"><action /><action /></machine>")
 (check-expect (parse '(start)) "<start />")
 (check-expect (parse '(server ((name "example.org"))))
               "<server name=\"example.org\" />")
